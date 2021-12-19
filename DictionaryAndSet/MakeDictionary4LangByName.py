54bddc929bdab3e65a855322f1608d0bb224a5a2
Qasim Attaullah <friday8580@gmail.com>
2021-12-19 13:07:28 +0500

    4friendLanguagePrint

---
+++ DictionaryAndSet/MakeDictionary4LangByName.py
@@ -0,0 +1,11 @@
+favLength={}
+a=input("Enter your favourit language Zain\n")
+b=input("Enter your favourit language Qasim\n")
+c=input("Enter your favourit language Adeel\n")
+d=input("Enter your favourit language Saad\n")
+favLength['Zain']=a
+favLength['Qasim']=b
+favLength['Adeel']=c
+favLength['Saad']=d
+
+print(favLength)
