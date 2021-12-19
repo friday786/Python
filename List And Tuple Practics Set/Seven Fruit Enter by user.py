56f6be88158ca32116b2549f95f076175c7f57d6
Qasim Attaullah <friday8580@gmail.com>
2021-12-19 11:52:27 +0500

    List and Tuple Practics Set

---
+++ List And Tuple Practics Set/Seven Fruit Enter by user.py
@@ -0,0 +1,9 @@
+f1=input("Enter A Fruit Name 1: ")
+f2=input("Enter A Fruit Name 3: ")
+f3=input("Enter A Fruit Name 3: ")
+f4=input("Enter A Fruit Name 4: ")
+f5=input("Enter A Fruit Name 6: ")
+f6=input("Enter A Fruit Name 6: ")
+f7=input("Enter A Fruit Name 7: ")
+myFruitsList=[f1,f2,f3,f4,f5,f6,f7]
+print(myFruitsList)
