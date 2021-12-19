b6ceb13fa54d7ae3d2b676bec6518cd671156f36
Qasim Attaullah <friday8580@gmail.com>
2021-12-19 12:49:45 +0500

    UrdutoEnglishDictionary

---
+++ DictionaryAndSet/MakeDictionaryUrdutoEnglish.py
@@ -0,0 +1,11 @@
+myDict={
+    "Panka":"Fan",
+    "Dabba":"Box",
+    "Kapra":"Clothes",
+    "Jota":"Shoes",
+    "Mosam":"Season",
+    "Sarde":"Cold"    
+}
+print("Option are ",myDict.keys())
+a=input("Enter the Urdu Word\n ")
+print("The meaning of your word is: ",myDict[a])
