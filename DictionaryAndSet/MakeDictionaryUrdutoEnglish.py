3ce2c5b8f1c45841066298dddc0c477577f9ab5a
Qasim Attaullah <friday8580@gmail.com>
2021-12-19 12:58:07 +0500

    UniqueNumberprintUsingSet

--- DictionaryAndSet/MakeDictionaryUrdutoEnglish.py
+++ DictionaryAndSet/MakeDictionaryUrdutoEnglish.py
@@ -8,4 +8,5 @@ myDict={
 }
 print("Option are ",myDict.keys())
 a=input("Enter the Urdu Word\n ")
-print("The meaning of your word is: ",myDict[a])
+#Below line will not throw an error if the key is not present in the dictionary
+print("The meaning of your word is: ",myDict.get(a))
