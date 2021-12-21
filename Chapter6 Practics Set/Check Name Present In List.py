7ab9c621316abd538dc887486f291ed365b3f416
Qasim Attaullah <friday8580@gmail.com>
2021-12-21 14:53:39 +0500

    name present from list

---
+++ Chapter6 Practics Set/Check Name Present In List.py
@@ -0,0 +1,7 @@
+Name = ["qasim","zain","harry","usman","uzair"]
+name=input("Enter the name to check\n")
+
+if name in Name:
+    print("Your name is present in List")
+else:
+    print("Your name is not present in List")    
