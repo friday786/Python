f453334a15e0e68b116f32b8353676fd64c7b20f
Qasim Attaullah <friday8580@gmail.com>
2022-01-12 14:40:42 +0500

    Reverse Table using For loop

---
+++ Loops/Print Table in Reverse Order.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a Number: "))
+for i in range(10,0,-1):
+    print(str(num) + " X " + str(i) + "=" +str(i*num))
+    
+    
+#While Loop
+# num = int(input("enter the number= "))
+# i=10
+# while i>=1:
+#     print(num,"X",i,"=",num*i)
+#     i= i-1    
+    
